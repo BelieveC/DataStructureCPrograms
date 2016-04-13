02dfa3a09d4e104891d51aa2932e0184d107441c
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-13 07:18:25 +0530

    DIsjoint sets added

--- DisjointSetMakeSet.c
+++ DisjointSetMakeSet.c
@@ -1,9 +1,68 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-struct node{
-    int value;
+struct subset{
     int rank;
-    struct node* parent;
-};
-void make_set())
+    int parent;
+}typedef subset;
+
+int find_set(subset subset[],int i)
+{
+    if(subset[i].parent!=i){
+        find_set(subset,subset[i].parent);
+    }
+    else{
+        return i;
+    }
+}
+
+void make_set(subset subset[],int i)
+{
+    subset[i].rank = 0;
+    subset[i].parent = i;
+}
+
+void make_union(subset subset[],int x,int y)
+{
+    if(subset[x].rank>subset[y].rank){
+        int p1 = find_set(subset,y);
+        int p2 = find_set(subset,x);
+        subset[p1].parent = p2;
+    }
+    else if(subset[x].rank<subset[y].rank){
+        int p1 = find_set(subset,y);
+        int p2 = find_set(subset,x);
+        subset[p2].parent = p1;
+    }
+    else{
+        int p1 = find_set(subset,y);
+        int p2 = find_set(subset,x);
+        subset[p1].parent = p2;
+        subset[p2].rank++;
+    }
+}
+
+int main()
+{
+    subset* sub = (subset*)malloc(10*sizeof(subset));
+    int i;
+    printf("I am here\n");
+    for(i=1;i<=10;i++){
+        make_set(sub,i);
+    }
+
+    for(i=1;i<=10;i++){
+        printf("%d %d\n",sub[i].parent,sub[i].rank);
+    }
+    make_union(sub,1,2);
+    make_union(sub,2,3);
+    make_union(sub,3,4);
+    for(i=1;i<=10;i++){
+        printf("%d %d\n",sub[i].parent,sub[i].rank);
+    }
+    for(i=1;i<=10;i++){
+        printf("Parent of %d is %d\n",i,find_set(sub,i));
+    }
+    return 0;
+}
+
