2fa6e6faa06a5c37e35e16c6bf44ed2b65c3dd44
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-30 13:29:40 +0530

    Dijkstra Bug

--- dijsktra.c
+++ dijsktra.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include<limits.h>
-#define V 9
+#define V 3
 int mindis(int dist[],int sptSet[])
 {
     int min = INT_MAX,min_index;
     int i;
     for(i=0;i<V;i++){
-        if(dist[i]<min && sptSet[i]==0){
+        if(dist[i] < min && sptSet[i]==0){
             min = dist[i],min_index = i;
         }
     }
+    printf("I am in Mindis\n");
     return min_index;
 }
 void print(int dist[])
@@ -29,14 +30,14 @@ void dijsktra(int src,int arr[V][V])
     }
     int sptSet[V];
     for(i=0;i<V;i++){
-        sptSet[V] = 0;
+        sptSet[i] = 0;
     }
     dist[src] = 0;
     for(i=0;i<V-1;i++){
         int min_index = mindis(dist,sptSet);
         sptSet[min_index] = 1;
         for(i=0;i<V;i++){
-            if(dist[i]>dist[min_index]+ arr[min_index][i]){
+            if(dist[i]>dist[min_index]+ arr[min_index][i] && !sptSet[i] && arr[min_index][i]){
                 dist[i] = dist[min_index]+arr[min_index][i];
             }
         }
@@ -50,9 +51,11 @@ int main()
     scanf("%d",&n);
     int arr[20][20];
     int i,j;
-    for(i=1;i<=n;i++){
+    for(i=0;i<n;i++){
         for(j=1;j<=n;j++){
             scanf("%d",&arr[i][j]);
         }
     }
+    dijsktra(0,arr);
+    return 0;
 }
