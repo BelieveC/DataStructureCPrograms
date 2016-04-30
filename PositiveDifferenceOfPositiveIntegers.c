3d525737d22e0d9e32084989c4a00a2467bb0481
Chaitanya Yadav <cayadav2009@gmail.com>
2016-05-01 02:11:03 +0530

    Question Added

---
+++ PositiveDifferenceOfPositiveIntegers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int diff(int arr[],int x,int y)
+{
+    int k = arr[x]-arr[y];
+    if(k<0){
+        return k*(-1);
+    }
+    return k;
+}
+int main()
+{
+    int arr[] = {57393,29849,9283839,20202,2898929};
+    printf("%d",diff(arr,1,3));
+    return 0;
+}
